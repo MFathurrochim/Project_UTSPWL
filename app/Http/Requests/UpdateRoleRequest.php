8ae7c40a35c148f9d799a16299c36b12263c215e
ViviErlina <h1101231010@student.untan.ac.id>
2025-03-19 22:32:32 +0700

    melakukan tahap 17-18

--- app/Http/Requests/UpdateRoleRequest.php
+++ app/Http/Requests/UpdateRoleRequest.php
@@ -7,7 +7,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 
-class UpdateProductRequest extends FormRequest
+class UpdateRoleRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -24,8 +24,8 @@ class UpdateProductRequest extends FormRequest
     public function rules(): array
     {
         return [
-            'name' => 'required|string|max:250',
-            'description' => 'required|string'
+            'name' => 'required|string|max:250|unique:roles,name,' . $this->role->id,
+            'permissions' => 'required',
         ];
     }
 }
--- app/Http/Requests/UpdateUserRequest.php
+++ app/Http/Requests/UpdateUserRequest.php
@@ -7,7 +7,7 @@ namespace App\Http\Requests;
 use Illuminate\Foundation\Http\FormRequest;
 
 
-class UpdateProductRequest extends FormRequest
+class UpdateUserRequest extends FormRequest
 {
     /**
      * Determine if the user is authorized to make this request.
@@ -19,13 +19,15 @@ class UpdateProductRequest extends FormRequest
     /**
      * Get the validation rules that apply to the request.
      *
-     * @return array<string,\Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
      */
     public function rules(): array
     {
         return [
             'name' => 'required|string|max:250',
-            'description' => 'required|string'
+            'email' => 'required|string|email:rfc,dns|max:250|unique:users,email,' . $this->user->id,
+            'password' => 'nullable|string|min:8|confirmed',
+            'roles' => 'required'
         ];
     }
 }
